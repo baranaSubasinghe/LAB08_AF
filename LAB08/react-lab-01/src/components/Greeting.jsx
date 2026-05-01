140be95a469b91452e807bcd9345c2068db8a031
Barana <baranapabasara@gmail.com>
2026-05-01 20:46:55 +0530

    Complete Exercise 01 Greeting component

--- LAB08/react-lab-01/src/components/Greeting.jsx
+++ LAB08/react-lab-01/src/components/Greeting.jsx
@@ -1,7 +1,7 @@
 function Greeting() {
   return (
     <div className="greeting">
-      <h1>Hello World!</h1>
+      <h1> Hello World !</h1>
       <p>Welcome to my first React lab project.</p>
     </div>
   );
